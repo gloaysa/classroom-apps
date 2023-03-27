b27258739d3621f0e9ec35dea981645d92137cb4
Guillermo Loaysa <gloaysa@gmail.com>
2023-03-27 10:41:35 +0200

    feat(): basic error handling in server ws connections

--- src/server/routes/ws/ws.route.ts
+++ src/server/routes/ws/ws.route.ts
@@ -17,99 +17,115 @@ export const WsRoute = (expressWs: express_ws.Instance) => {
 };
 
 function handleOnWsMessage(ws: ws, req: express.Request, getWss: () => ws.Server, msg: string) {
-	const userFromQuery = JSON.parse(req.query.user?.toString() ?? '');
-	const user: User | undefined = userService.getUserById(userFromQuery?.id);
-	if (!user) {
+	try {
+		const userFromQuery = JSON.parse(req.query.user?.toString() ?? '');
+		const user: User | undefined = userService.getUserById(userFromQuery?.id);
+		const message: IWsMessage | undefined = JSON.parse(msg);
+
+		if (!message || !user) {
+			console.warn(`No user or message in message - user: ${user?.id} | msg: ${msg}`);
+			return;
+		}
+
+		console.log(`New message from ${user.name} - ${user.id}: ${msg}`);
+
+		switch (message.type) {
+			case WsMessageType.NewGame:
+				userService.addUserToGame(user.id, message.data.gameName, true);
+				break;
+			case WsMessageType.JoinGame:
+				userService.addUserToGame(user.id, message.data.gameName);
+				ws.send(
+					JSON.stringify({
+						type: WsMessageType.User,
+						data: userService.getUserById(user.id),
+					})
+				);
+				break;
+			case WsMessageType.BuzzerOnOff:
+				if (message.data?.state) {
+					userService.resetBuzzed(user.gameId);
+				}
+				broadcastMessage(
+					{
+						type: WsMessageType.BuzzerOnOff,
+						data: message.data.state,
+					},
+					getWss
+				);
+				break;
+			case WsMessageType.Buzzed:
+				userService.userBuzzed(user.id, user.gameId, true);
+				break;
+			case WsMessageType.UserLogout:
+				userService.deleteUser(user);
+				console.info(`User ${user.name} logged out`);
+				break;
+		}
+
+		const connectedUser = userService.getUserById(user.id);
+		const host = userService.getGameHost(connectedUser?.gameId);
+		const response = {
+			type: WsMessageType.ForHost,
+			data: host,
+		};
+		broadcastMessage(response, getWss);
+	} catch (e) {
+		console.error(`There was an error processing last message: ${msg} - ${e}`);
 		return;
 	}
-
-	const message: IWsMessage = JSON.parse(msg);
-	console.log(message);
-	switch (message.type) {
-		case WsMessageType.NewGame:
-			userService.addUserToGame(user.id, message.data.gameName, true);
-			break;
-		case WsMessageType.JoinGame:
-			userService.addUserToGame(user.id, message.data.gameName);
-			ws.send(
-				JSON.stringify({
-					type: WsMessageType.User,
-					data: userService.getUserById(user.id),
-				})
-			);
-			break;
-		case WsMessageType.BuzzerOnOff:
-			if (message.data?.state) {
-				userService.resetBuzzed(user.gameId);
-			}
-			broadcastMessage(
-				{
-					type: WsMessageType.BuzzerOnOff,
-					data: message.data.state,
-				},
-				getWss
-			);
-			break;
-		case WsMessageType.Buzzed:
-			userService.userBuzzed(user.id, user.gameId, true);
-			break;
-		case WsMessageType.UserLogout:
-			userService.deleteUser(user);
-			console.info(`User ${user.name} removed`);
-			break;
-	}
-
-	const connectedUser = userService.getUserById(user.id);
-	const host = userService.getGameHost(connectedUser?.gameId);
-	const response = {
-		type: WsMessageType.ForHost,
-		data: host,
-	};
-	broadcastMessage(response, getWss);
 }
 
 function handleOnWsConnect(ws: ws, req: express.Request, getWss: () => ws.Server) {
 	const userFromQuery = req.query.user as string;
-	console.log(userFromQuery);
 	if (!userFromQuery) {
 		ws.close(403);
 	}
-	const user: User = JSON.parse(userFromQuery);
-	console.log(`Received a new connection.`);
+	try {
+		const user: User = JSON.parse(userFromQuery);
+		console.log(userFromQuery);
+		const newUser = userService.createNewUser(user.id, user.name);
+		console.info(`${user.name} connected with id ${user.id}.`);
 
-	// Store the new connection and handle messages
-	const newUser = userService.createNewUser(user.id, user.name);
-	console.log(`${user.name} connected.`);
-	const response = {
-		type: WsMessageType.NewUser,
-		data: newUser,
-	};
-	ws.send(JSON.stringify(response));
+		const response = {
+			type: WsMessageType.NewUser,
+			data: newUser,
+		};
+		ws.send(JSON.stringify(response));
 
-	const host = userService.getGameHost(newUser?.gameId);
-	const messageForHost = {
-		type: WsMessageType.ForHost,
-		data: host,
-	};
-	broadcastMessage(messageForHost, getWss);
+		const host = userService.getGameHost(newUser?.gameId);
+		const messageForHost = {
+			type: WsMessageType.ForHost,
+			data: host,
+		};
+		broadcastMessage(messageForHost, getWss);
+	} catch (e) {
+		console.error(`There was an error connecting new user: ${userFromQuery} - ${e}`);
+		return;
+	}
 }
 
 function handleOnWsClose(req: express.Request, getWss: () => ws.Server) {
-	const userFromQuery = JSON.parse(req.query.user?.toString() ?? '');
+	try {
+		const userFromQuery = JSON.parse(req.query.user?.toString() ?? '');
 
-	const user: User | undefined = userService.getUserById(userFromQuery?.id);
-	if (!user) {
+		const user: User | undefined = userService.getUserById(userFromQuery?.id);
+		if (!user) {
+			return;
+		}
+		userService.disconnectUser(user);
+
+		console.log('disconnect', user.name);
+		const host = userService.getGameHost(user?.gameId);
+		const messageForHost = {
+			type: WsMessageType.ForHost,
+			data: host,
+		};
+		broadcastMessage(messageForHost, getWss);
+	} catch (e) {
+		console.error(`There was an error disconnecting user ${req.query.user}, ${e}`);
 		return;
 	}
-	userService.disconnectUser(user);
-
-	console.log('disconnect', user.name);
-	const host = userService.getGameHost(user?.gameId);
-	const messageForHost = {
-		type: WsMessageType.ForHost,
-		data: host,
-	};
-	broadcastMessage(messageForHost, getWss);
 }
 
 function broadcastMessage(json: IWsMessage, getWss: () => ws.Server) {
